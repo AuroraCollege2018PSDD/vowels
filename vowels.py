5143676d830cd87bed3f67ee0421095e87ae8dc0
Geoff Goldrick <geoff.goldrick@det.nsw.edu.au>
2018-06-14 09:04:49 +1000

    Add files via upload
    added many comments

--- vowels.py
+++ vowels.py
@@ -18,12 +18,14 @@ import sys  # to communicate with windows
 P.init()  # starts the game engine
 clock = P.time.Clock()  # creates clock to limit frames per second
 loopRate = 60  # sets max speed of main loop
+
 SCREENSIZE = SCREENWIDTH, SCREENHEIGHT = 1024, 768  # sets size of screen/window
-DEFAULT_TEXT_SIZE = 48
 screen = P.display.set_mode(SCREENSIZE)  # creates window and game screen
+
+DEFAULT_TEXT_SIZE = 48 #a default for any rendered text
 P.display.set_caption('Know your vowels!')
-defaultFont = P.font.Font(None,80) #create a font for the letters, default font
-selectedFont = P.font.Font(None,40) #a smaller font for after the letters have been selected
+#defaultFont = P.font.Font(None,80) #create a font for the letters, default font
+#selectedFont = P.font.Font(None,40) #a smaller font for after the letters have been selected
 
 # set variables for some colours RGB (0-255)
 white = (255, 255, 255)
@@ -50,20 +52,20 @@ class renderedLetter(object):
         """ inits the letter"""
         self.text = letter
         self.size = DEFAULT_TEXT_SIZE
-        self.color = blue
-        self.backColor = black
-        self.x = 10
-        self.y = 100
-        #self.font = P.font.Font(None,self.size)
+        self.color = blue #text color
+        self.backColor = black #text background color
+        self.x = 10 #where across the screen it is drawn
+        self.y = 100 #where down the screen letter is rendered
         self.font = P.font.SysFont("Lucida Console", self.size) #rendered best with monospace font
-        self.renderedText = self.font.render(self.text,1,self.color,self.backColor)
-        self.rectangle = self.renderedText.get_rect().move(self.x,self.y)
+        self.renderedText = self.font.render(self.text,1,self.color,self.backColor) #actually create the rendered text
+        self.rectangle = self.renderedText.get_rect().move(self.x,self.y) #need to know the rectangle of text to interact
         
     def update(self):
-        #self.font = P.font.Font(None,self.size)
+        '''  update the rendered text and rectangle after changes
+        '''
         self.font = P.font.SysFont("Lucida Console", self.size) #rendered best with monospace font
-        self.renderedText = self.font.render(self.text,1,self.color,self.backColor)
-        self.rectangle = self.renderedText.get_rect().move(self.x,self.y)
+        self.renderedText = self.font.render(self.text,1,self.color,self.backColor) #actually create the rendered text
+        self.rectangle = self.renderedText.get_rect().move(self.x,self.y) #need to know the rectangle of text to interact
         
 #define other setup variables
 alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -72,7 +74,7 @@ vowels = "aeiou"
 #create arrays to display the rendered letters
 alphabetArray = [] #an initially empty array
 for letter in alphabet:
-    rLetter = renderedLetter(letter)
+    rLetter = renderedLetter(letter) #create an instance of the class renderedLetter
     alphabetArray.append(rLetter)
     
 vowelArray = [] #an initially empty array
@@ -80,16 +82,17 @@ for letter in vowels:
     rLetter = renderedLetter(letter)
     vowelArray.append(rLetter)
 
-xPosition = 10
-yPosition = 400
+#draw alphabet letters on the screen
+xPosition = 10 #across
+yPosition = 400 #down
 for l in alphabetArray:
-    l.x = xPosition
-    l.y = yPosition
-    l.color = blue
+    l.x = xPosition #set the x position of the rendered letter
+    l.y = yPosition #set the y position of the rendered letter
+    l.color = blue #start with letters blue
     l.backColor = purple
-    l.update()
-    screen.blit(l.renderedText,l.rectangle)
-    xPosition += (l.rectangle.width + 10)
+    l.update() #changed the properties so need to update
+    screen.blit(l.renderedText,l.rectangle) #tell pygame where to draw next screen is flipped
+    xPosition += (l.rectangle.width + 10) #move the x position to the right so letters not drawn on top of each other
 
 xPosition = 320
 yPosition = 200
@@ -98,11 +101,13 @@ for l in vowelArray:
     l.y = yPosition
     l.color = lightBlue
     l.backColor = lightBlue
-    l.size = 2 * DEFAULT_TEXT_SIZE
+    l.size = 2 * DEFAULT_TEXT_SIZE #make the vowels twice as big
     l.update()
     screen.blit(l.renderedText,l.rectangle)
     xPosition += (l.rectangle.width + 20)
 
+
+#everytihng up to here was setting up - now lets play
 
 play = True  # controls whether to keep playing
 
@@ -113,20 +118,22 @@ while play:  # game loop - note:  everything in this loop is indented one tab
         if event.type == P.QUIT:  # tests if window's X (close) has been clicked
             play = False  # causes exit of game loop
     # your code starts here ##############################
-        if event.type == P.MOUSEBUTTONDOWN:
-            mousePosition = P.mouse.get_pos()
+        elif event.type == P.MOUSEBUTTONDOWN:
+            mousePosition = P.mouse.get_pos() #find out where they clicked
+            
+            #need to check all letters to see if they clicked on that letter
             for a in alphabetArray:
-                if a.rectangle.collidepoint(mousePosition):
+                if a.rectangle.collidepoint(mousePosition): #is the mouse click inside the letters rectangle
                     a.color = red
                     #a.update()
-                    for v in vowelArray:
-                        if a.text == v.text:
+                    for v in vowelArray: #check whether that letter is a vowel
+                        if a.text == v.text: #compare text of clicked letter to the vowel text
                             a.color = green
                             v.backColor = purple
-                            v.update()
-                            screen.blit(v.renderedText, v.rectangle)
-                    a.update()
-                    screen.blit(a.renderedText, a.rectangle)
+                            v.update() #made changes so we need to update
+                            screen.blit(v.renderedText, v.rectangle) #need to re-blit
+                    a.update() #made changes so we need to update
+                    screen.blit(a.renderedText, a.rectangle) #need to re-blit
                 
 
 
@@ -138,3 +145,4 @@ while play:  # game loop - note:  everything in this loop is indented one tab
 # out of game loop ###############
 P.quit()   # stops the game engine
 sys.exit()  # close operating system window
+
